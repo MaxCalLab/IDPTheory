7d527c069fa6d588dbc7968679c33395db0cfe3c
KingsLab <80141144+MaxCalLab@users.noreply.github.com>
2023-08-25 16:49:07 -0600

    Update crit_checker.py

--- rG-RPA/crit_checker.py
+++ rG-RPA/crit_checker.py
@@ -7,10 +7,9 @@
 ##      4. which model ('standard' or 'selfderivs')
 ##      5. a file (path to sequence file; CSV)
 ##      6. directory path for saving critical point
-##  > Note: you can expect some adjustment of 'phi_bracket' and 'u_bracket' is required for each case.
+##  > Note: you can expect some adjustment of 'phi_bracket' and 'u_bracket' may be required for each case.
 
-# Make sure to import the intended 'Model' !!  [standard self-energy corr., better corr., or 'a priori' approach]
-import Sequence as S, Solver as V   #, Model_standard as M
+import Sequence as S, Solver as V, Model_standard as M
 import sys
 import os
 
@@ -23,30 +22,27 @@ INFO_ONLY = False
 #Fisher_choice = "Phos"
 Fisher_choice = None
 
-NO_POS = False       # neglect any positive charges in sequence
+NO_POS = False       # neglect any positive charges in sequence?
 #NO_POS = True
 if NO_POS:
     S.aminos.update({"R":0, "K":0})
 
-OLD_CRIT = False    # use _old_ critical point finder?  [simultaneous zero of d2F and d3F]
-#OLD_CRIT = True
 
 # defaults
-head_name = "NAME"
-head_seq = "SEQUENCE"
-#head_seq = "Seq"
-#afile = "../rG-RPA_coex/sequences/Fisher_phos_scd.csv"
-#afile = "../RPA_coex/sequences/Fisher_phos_scd_NEW-FIX.csv"
-afile = "../RPA_coex/sequences/Fisher_phos_scd_NEW-FINAL.csv"
-#afile = "../rG-RPA_coex/sequences/Fisher_KI67_repeats_xcheck.csv"
-#afile = "../rG-RPA_coex/sequences/RG_tests.csv"
-#afile = "../rG-RPA_coex/sequences/yamazaki.csv"
-seqname = "KI-67_cons"
-#pars = "cions+v2_fg"
-pars = {'cions': 1, 'v2': 4.1887902047863905, 'eps0': 2.0, 'mode': 'rG', 'mean-field':False}
-#pars = {'cions': 2, 'v2': 0, 'eps0': 0, 'mode': 'fG', 'ionsize': 'point', 'potential': 'short'}
-#pars = "cions+short+point_fg"
-which_model = "standard"
+afile = "./RG_tests.csv"        # file with sequence(s)
+#afile = "./Fisher_phos.csv"
+
+head_name = "NAME"        # heading in CSV file for sequence name
+head_seq = "SEQUENCE"     # heading in CSV file for sequence aminos
+
+seqname = "IP5"    # name of sequence in file
+
+#pars = "cions+salt+coulomb_fg"        # using code-string to set parameters
+
+SALT = 100    # salt concentration (milli-Molar)
+pars = {'cions': 1, 'salt':M.phi_s(SALT), 'v2': 4.1887902047863905, 'eps0': 2.0, 'mode': 'rG', 'mean-field':False}    # dictionary to set parameters
+
+#pars = {'cions': 1, 'v2': 0, 'eps0': 0, 'mode': 'fG', 'ionsize': 'point', 'potential': 'short'}    # other example
 
 
 # command line inputs
@@ -89,46 +85,17 @@ elif "yamazaki" in afile:
 else:
     alias=None
 
-#print("TESTING HEADING  :  " + head_seq)
 
-# load proper model
-which_model = which_model.lower()
-if which_model == "standard":
-    import Model_standard as M
-elif which_model == "selfderivs":
-    import Model_selfderivs as M
-elif which_model == "noself":
-    import Model_noself as M
-
-# adjustment for salt
-#SALT = 100      # salt concentration, in milli-Molar
-SALT = 0
-#phi_salt = SALT * (5.5**3) * (6.022e-4) * (1e-3)        # using Kuhn length
-phi_salt = SALT * (3.8**3) * (6.022e-4) * (1e-3)        # using bond length
-if SALT:
-    pars = str({"cions":1, "v2":(4*M.PI/3), "salt":phi_salt, "eps0":2.0, "mode":"rG"})
-
-# check for dictionary format of parameters
+# check for string-dictionary format of parameters
 if type(pars) == str:
     try:
         pars = eval(pars)
     except NameError:
         pars = pars
 
-#t1 = M.perf_counter()
+
 # get sequence object
 seq = S.Sequence(seqname, file=afile, alias=alias, headName=head_name, headSeq=head_seq)
-#seq.info(showSeq=True)
-#t2 = M.perf_counter()
-#print("\nSEQUENCE LOADING TIMER : {}\n".format(t2-t1))
-
-if INFO_ONLY:
-#    alpha = 1.18        # hydrophobics and aromatics
-#    beta = 19.78        #
-    alpha , beta = 2.36 , 0     # just hydrophobics
-    e = alpha*seq.frac_hp + beta*seq.fracp*seq.frac_ar
-    print("-> EXPECT eps0 = {:.5f}\n\n".format(e))
-    sys.exit()
 
 # get model object (rG-RPA)
 mod = M.RPA(seq, pars)
@@ -139,40 +106,20 @@ co = V.Coex(mod, spars={"thr":1e-6})
 
 # exit now if all you wanted was sequence information
 if INFO_ONLY:
-#    k2 = 2
-#    print("\nCorrelation / Structure values (at k2={:.3f}):".format(k2))
-#    print("\tG0 -> {:.5f}".format(mod.realG(k2, 1, mod.range0)))
-#    print("\tg0 -> {:.5f}".format(mod.realMinig(k2, 1, mod.range0)))
-#    print("\tZ0 -> {:.5f}".format(mod.realZ(k2, 1, mod.range0)))
-#    print("\tG2 -> {:.5f}".format(mod.realG(k2, 1, mod.range2)))
-#    print("\tg2 -> {:.5f}".format(mod.realMinig(k2, 1, mod.range2)))
-#    print("\tZ2 -> {:.5f}".format(mod.realZ(k2, 1, mod.range2)))
-#    print("\n")
     sys.exit()
 
 # find critical point
-if OLD_CRIT:
-#    co.OLD_find_crit()
-    co.OLD_find_crit(XintKW={"big":100.})
-else:
-#    PBrack = (1e-2/(seq.N**1.6),0.01/(seq.N**0.4), (0.1+0*(seq.N**(-0.08)))/(seq.N**0.08))
-#    PBrack = (M.np.exp(-seq.N**0.45),0.01/(seq.N**0.5), (0.1+0*(seq.N**(-0.08)))/(seq.N**0.08))
-#    PBrack = (1e-3, 4.9e-3, 1e-2)
-#    co.find_crit(phi_bracket=PBrack, u_bracket=(1e-4,1e4), u_bracket_notes=False)
-    print("Minimizing inverse: u=1/t.\n")
-    co.find_crit(phi_bracket=None, u_bracket=(1e-6,1e4), u_bracket_notes=False)
-    # below: use 'negative temperature' approach (with brent bracket)
-#    print("Minimizing negative: u=-t.\n")
-#    co.find_crit_NT(phi_bracket=None, u_bracket=(-1e6,-1e-4), u_bracket_notes=False)
+print("Minimizing inverse: u=1/t.\n")
+co.find_crit(phi_bracket=None, u_bracket=(1e-6,1e4), u_bracket_notes=False)
 
 (pc, tc) = co.get_crit()
 print("\t(phi_c, T_c) = ({:5.5g}, {:5.5g})\n".format(pc,tc))
 #print("\t(T_c, phi_c) = ({:5.5g}, {:5.5g})\n".format(tc,pc))
 
+
 # check that it's really a solution
 print("\nChecking that d2F and d3F are indeed close to zero at (phi_c,T_c):")
 
-#mod.info()
 
 if mod.pars["mode"][0].lower() == "r":
     x = mod.find_x(pc, tc)
